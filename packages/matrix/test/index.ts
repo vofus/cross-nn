b9d6e643ff60350d8f8c52b10640744a9b0bc439
Andrey Vasin <vofuss@gmail.com>
2020-04-26 12:07:07 +0400

    First version of Matrix package.

--- packages/matrix/test/index.ts
+++ packages/matrix/test/index.ts
@@ -1,10 +1,10 @@
-import { Matrix } from '../src/matrix';
+import { Matrix } from '../src/models/matrix';
 
 const m_01 = Matrix.fromArray([[1,2], [3,4]]);
 const m_02 = Matrix.fromArray([[1], [2]]);
 
-console.log(m_01);
-console.log(m_02);
+console.log(m_01.multiply(2));
+console.log(m_02.subtract(Matrix.fromArray([[1], [1]])));
 
 if (Boolean(m_01) && Boolean(m_02)) {
 	const result = m_01.dot(m_02);
