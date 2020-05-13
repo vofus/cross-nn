bff2784bf0083d7291b51ffe9cb4f53568a1d039
Andrey Vasin <vofuss@gmail.com>
2020-05-13 11:09:31 +0400

    BROWSER_MODULE: Change build tool configuration (0.0.7).

--- browser-demo/src/app/pages/main/main-page/main-page.component.ts
+++ browser-demo/src/app/pages/main/main-page/main-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-// import { BrowserAdapter } from '@cross-nn/browser';
+import { BrowserAdapter } from '../../../../../../packages/browser';
 import { ModelEditorService } from '@shared/model-editor/model-editor.service';
+import { LearningGradAlgorithm, NeuralNetwork, NeuralNetworkConfig, TrainItem } from '@cross-nn/core';
 
 @Component({
   selector: 'app-main-page',
@@ -8,7 +9,7 @@ import { ModelEditorService } from '@shared/model-editor/model-editor.service';
   styleUrls: ['./main-page.component.scss']
 })
 export class MainPageComponent implements OnInit {
-  // private nnAdapter: BrowserAdapter = null;
+  private nnAdapter: BrowserAdapter = null;
   public threadsConfig = {
     count: 1,
     changed: false
@@ -40,6 +41,51 @@ export class MainPageComponent implements OnInit {
 
   public updateBrowserAdapter() {
     this.threadsConfig.changed = false;
+    // this.nnAdapter = new BrowserAdapter('/workers/cross-nn.worker.js', this.threadsConfig.count);
+    // console.log('ADAPTER: ', this.nnAdapter);
+
+    const adapter = new BrowserAdapter('workers/cross-nn.worker.js', this.threadsConfig.count);
+    const epochCount = 10;
+
+    const nnConfig: NeuralNetworkConfig = {
+      neuronCounts: [2, 30, 50, 30, 1],
+      learningRate: 0.3,
+      moment: 0.3
+    };
+
+    const trainSet: TrainItem[] = [
+      {inputs: [0, 1], targets: [1]},
+      {inputs: [1, 0], targets: [1]},
+      {inputs: [0, 0], targets: [0]},
+      {inputs: [1, 1], targets: [0]}
+    ];
+
+    const scaledTrainSet: TrainItem[] = [];
+    for (let i = 0; i < 100; ++i) {
+      scaledTrainSet.push(...trainSet);
+    }
+
+
+    // setTimeout(() => {
+      for (let i = 0; i < 4; ++i) {
+        adapter.gradAlgorithmTrainAsync(
+          new NeuralNetwork(nnConfig),
+          LearningGradAlgorithm.BACK_PROP,
+          scaledTrainSet,
+          epochCount,
+          ({epochNumber, epochTime}) => {
+            const percent = Math.ceil((epochNumber/epochCount) * 100);
+            console.log('=== ' + i + ' === ' + `Complete percent: ${percent}% (${epochTime}ms)`);
+          }
+        ).then((nn) => {
+          console.log('=== ' + i + ' ===' + '[0, 1]: ', nn.query([0, 1]).toArray()[0]);
+          console.log('=== ' + i + ' ===' + '[1, 0]: ', nn.query([1, 0]).toArray()[0]);
+          console.log('=== ' + i + ' ===' + '[0, 0]: ', nn.query([0, 0]).toArray()[0]);
+          console.log('=== ' + i + ' ===' + '[1, 1]: ', nn.query([1, 1]).toArray()[0]);
+        })
+          .catch(console.error);
+      }
+    // }, 0);
   }
 
   public async createNewModel() {
---
+++ browser-demo/src/workers/cross-nn.worker.js
@@ -0,0 +1,2 @@
+// This file will be replaced by @cross-nn/browser/dist/worker.js
+// More details: angular.json/build/fileReplacements
